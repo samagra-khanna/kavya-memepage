72618dd683f97bab44186580ae22fcbab816c9e4
Samagra Khanna <85937988+samagra-khanna@users.noreply.github.com>
2022-12-22 22:43:45 +0530

    commit

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,4 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
-window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
-    loader.classList.add("loader-hidden");
-    loader.addEventListener("transitionend", () => {
-        loader.style.display = "none";
-    });
-});
 function checkFileFormat() {
     var fileName = document.getElementById('meme-post').value.toLowerCase();
     if (!fileName.endsWith('.jpg') && !fileName.endsWith('.jpeg') && !fileName.endsWith('.gif') && !fileName.endsWith('.jfif') && !fileName.endsWith('.png')) {
@@ -13,3 +6,16 @@ function checkFileFormat() {
         window.location.reload();
     }
 }
+const form = document.querySelector('form');
+
+$("#meme-post").bind('change', function () {
+    var filename = $("#meme-post").val();
+    if (/^s*$/.test(filename)) {
+        $("#blankFile").text("No File Chosen..");
+        $(".success").hide();
+    } else {
+        $("#blankFile").text(filename.replace("C:\\fakepath\\", ""));
+        $(".success").show();
+        $(form).submit();
+    }
+}); 
